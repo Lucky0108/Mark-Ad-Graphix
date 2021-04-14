81af04612cada64164756130a5a6c2be8555a4ac
Lucky0108 <raolakshayyadav02@gmail.com>
2021-04-15 02:06:38 +0530

    Added carousels in clients and products section of home

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,9 @@ import Client from './Containers/Clients';
 import Contact from './Containers/Contact';
 import Footer from './Components/Footer';
 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 function App() {
   return (
     <div className="App">
--- client/src/Containers/Home/index.jsx
+++ client/src/Containers/Home/index.jsx
@@ -6,6 +6,16 @@ import { Container, Row } from 'react-bootstrap';
 import HomeServiceCard from '../../Components/HomeServiceCard';
 import { Link } from 'react-router-dom';
 import HomeSectionsHeading from '../../Components/HomeSectionsHeading';
+import pro1 from '../../img/download.jfif'
+import 'swiper/swiper-bundle.css'
+// import Swiper core and required modules
+import SwiperCore, { Pagination,  Autoplay, EffectFade } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// install Swiper modules
+SwiperCore.use([Pagination, Autoplay, EffectFade]);
+
 /**
 * @author
 * @function Home
@@ -15,17 +25,17 @@ const Home = (props) => {
   return (
     <>
 
-    {/* Hero Section */}
+      {/* Hero Section */}
       <section className="home-hero-section">
         <div className="hero-section-wrap">
           <video src={introVid} className="introVideo" loop muted autoPlay />
           <div className="contentDiv">
-          <div className="layer">
-            <span>WE HAVE PARTNER NETWORK SPREAD ACROSS INDIA</span>
+            <div className="layer">
+              <span>WE HAVE PARTNER NETWORK SPREAD ACROSS INDIA</span>
+            </div>
+            <div className="mouse-effect"></div>
           </div>
-          <div className="mouse-effect"></div>
         </div>
-          </div>
       </section>
 
       {/* Services Section */}
@@ -48,33 +58,88 @@ const Home = (props) => {
 
       {/* About Section */}
       <section className="home-about-section">
-      <div className="home-about-wrap">
-      <video src={aboutVid} className="aboutVideo" loop muted autoPlay />
-      <div className="contentDiv">
-        <div className="layer">
-          <Container>
-            <Row>
-            <HomeSectionsHeading heading="The Signage Difference" para="Signage boasts over 58 years of experience implementing brands across a host of industries. The difference is our people and our processes. From healthcare to security, retail to automotive, our team consists of experts experienced within each of these fields, who understand our customers’ expectations and desires. Watch our video to learn more about The Signage Difference." />
-            </Row>
-            <div className="video-btn">
-            <i className="fa fa-play fa-2x" aria-hidden="true"></i>
+        <div className="home-about-wrap">
+          <video src={aboutVid} className="aboutVideo" loop muted autoPlay />
+          <div className="contentDiv">
+            <div className="layer">
+              <Container>
+                <Row>
+                  <HomeSectionsHeading heading="The Signage Difference" para="Signage boasts over 58 years of experience implementing brands across a host of industries. The difference is our people and our processes. From healthcare to security, retail to automotive, our team consists of experts experienced within each of these fields, who understand our customers’ expectations and desires. Watch our video to learn more about The Signage Difference." />
+                </Row>
+                <div className="video-btn">
+                  <i className="fa fa-play fa-2x" aria-hidden="true"></i>
+                </div>
+                <div className="home-common-btn-div">
+                  <Link to="/about" className="home-common-btn">Know More</Link>
+                </div>
+              </Container>
             </div>
-            <div className="home-common-btn-div">
-            <Link to="/about" className="home-common-btn">Know More</Link>
-            </div>
-          </Container>
+          </div>
         </div>
-      </div>
-      </div>
       </section>
 
       {/* Products Section */}
-      <section className="home-product-section section-padding">
+      <section className="home-product-section">
         <div className="home-product-wrap">
           <div className="layer">
             <Container>
               <Row>
-              <HomeSectionsHeading heading="Our Products" para="We offer a wide gamut of virtual advertising products including signboards, indoor reception signs, name plates, metal signs, ACP signs, directional/wayfinding, information directory, frosted films, LED display boards, LED signs, Corian and other miscellaneous jobs." />
+                <HomeSectionsHeading heading="Our Products" para="We offer a wide gamut of virtual advertising products including signboards, indoor reception signs, name plates, metal signs, ACP signs, directional/wayfinding, information directory, frosted films, LED display boards, LED signs, Corian and other miscellaneous jobs." />
+                <div className="product-carousel col-md-12">
+                <Swiper 
+                  className="product-swiper"
+                  effect="fade"
+                  direction="horizontal"
+                  slidesPerView={1}
+                  onSwiper={(swiper) => console.log(swiper)}
+                  pagination={{ clickable: true }}
+                  // autoplay = {{ delay: 2500, disableOnInteraction: false}}
+                >
+                  <SwiperSlide>
+                    <div className="product-slider-img">
+                      <img src={pro1} alt=" " />
+                    </div>
+                    <div className="product-slider-content">
+                      <div className="product-slider-title">
+                          <Link to="/portfolio">Wayfinding Signs & Totems</Link>
+                      </div>
+                      <div className="product-slider-text">
+
+                      Way finding systems are unique to each place and visitors. The function of a sign is to identify, inform, direct, honor, restrict or permit. A good sign system recedes into the background while providing clear information when needed. Successfully designed signage helps visitors find their way, makes information accessible, provides an enhanced experience. In addition, an inclusive assessment of the environment and issues that affect orientation for first time visitors…									
+                      </div>
+                      <Link to="/client" className="home-common-btn">View All</Link>
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <div className="product-slider-img">
+                      <img src={pro1} alt=" " />
+                    </div>
+                    <div className="product-slider-content">
+                      <div className="product-slider-title">
+                          <Link to="/portfolio">Acp Signs</Link>
+                      </div>
+                      <div className="product-slider-text">
+           						ACP signs is a premium option over basic sign boards wherein one can have a premium looking signage for his business along with comuterised cut letters &amp; logos LED lit. It offers an extensive range of basic colors to wooden shades to marble shades to even earthy shades. ACP signs is a premium option over basic sign boards wherein one can have a premium looking signage for his business along…									
+                      </div>
+                      <Link to="/client" className="home-common-btn">View All</Link>
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <div className="product-slider-img">
+                      <img src={pro1} alt=" " />
+                    </div>
+                    <div className="product-slider-content">
+                      <div className="product-slider-title">
+                          <Link to="/portfolio">LED Signs</Link>
+                      </div>
+                      <div className="product-slider-text">
+                      A LED letter sign is a three-dimensional sign element. It is fabricated from stainless steel and acrylic, thus it will not rust. LED letters are eye-catching and the most preferred choice when it comes to exterior signs. These signs are usually used as fixtures on parts of a building. LED letter signs enhance the overall look of the sign itself and add a lighting effect on the surrounding building façade...
+                      </div>
+                      <Link to="/client" className="home-common-btn">View All</Link>
+                    </div>
+                  </SwiperSlide>
+                </Swiper>
+                </div>
               </Row>
             </Container>
           </div>
@@ -82,15 +147,180 @@ const Home = (props) => {
       </section>
 
       {/* Clients Section */}
-      <section className="home-client-section section-padding">
+      <section className="home-client-section">
         <div className="home-client-wrap">
           <div className="layer">
             <Container>
               <Row>
-              <HomeSectionsHeading heading="Our Clients" para="We have proudly served wide spectrum industries such as real estate,education and training, finance companies, manufacturing, schools and colleges, healthcare, news channels, restaurant & hospitality sector, banks, MNCs as well as government organisations including MTNL, Bank of Baroda, ONGC, NDMC, etc.." />
+                <HomeSectionsHeading heading="Our Clients" para="We have proudly served wide spectrum industries such as real estate,education and training, finance companies, manufacturing, schools and colleges, healthcare, news channels, restaurant & hospitality sector, banks, MNCs as well as government organisations including MTNL, Bank of Baroda, ONGC, NDMC, etc.." />
+                <div className="client-carousel col-md-12">
+                <Swiper
+                  spaceBetween={30}
+                  slidesPerView={5}
+                  slidesPerColumn={2}
+                  slidesPerGroup={5}
+                  slidesPerColumnFill="row"
+                  grabCursor={true}
+                  onSwiper={(swiper) => console.log(swiper)}
+                  pagination={{ clickable: true }}
+                  autoplay = {{ delay: 2500, disableOnInteraction: false}}
+                  breakpoints = {{ 320: { slidesPerView: 2}, 500: { slidesPerView: 3}, 1001: { slidesPerView: 5} }}
+                >
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/Untitled-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/omaxe.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/ashok.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/ansal.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/birlasoft-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/alpha-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/amway-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/conver.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/dlf-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/apollo.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/apollo-school.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/efunds-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/signage-clients.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/lcacoste.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/tata.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/educomp-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/gulshan-1.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/central.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/toshiba.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/shroff.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/modern.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/malasiye.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/ndmc.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/shimz.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+                  <SwiperSlide>
+                  <div className="client-logo">
+                  <img src="https://signageindia.in/wp-content/uploads/2020/03/aditya.png" class="img-responsive" alt=" "/>
+                  </div> 
+                  </SwiperSlide>
+              </Swiper>
+                </div>
               </Row>
               <div className="home-common-btn-div">
-              <Link to="/client" className="home-common-btn">View All</Link>
+                <Link to="/client" className="home-common-btn">View All</Link>
               </div>
             </Container>
           </div>

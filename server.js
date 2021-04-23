9bac501f10bbf2925c4e02c8f350c4560ec3b68d
Lucky0108 <raolakshayyadav02@gmail.com>
2021-04-23 16:32:07 +0530

    test

--- server.js
+++ server.js
@@ -14,3 +14,9 @@ if(process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
     });
 }
+
+const port = process.env.PORT || 4000;
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`)
+})

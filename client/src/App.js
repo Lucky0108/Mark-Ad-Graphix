4ea8136706e06a8ecfbbf805b055425ba30cdab1
Lucky0108 <raolakshayyadav02@gmail.com>
2021-04-23 15:17:47 +0530

    Added Lazy Loading and server file

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,40 @@
-import { useEffect } from 'react';
-
 // Basic Imports
+import { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route, useLocation } from 'react-router';
 
 // Css Imports
 import './App.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import loadingImg from './img/puff.svg'
 
 // Route Components Import
-import Navbar from './Components/Navbar';
-import Home from './Containers/Home';
-import Service from './Containers/Services';
-import About from './Containers/About';
-import Product from './Containers/Products';
-import Client from './Containers/Clients';
-import Contact from './Containers/Contact';
-import Footer from './Components/Footer';
-import ProductPage from './Components/UI/ProductPage';
-import WhatsAppIcon from './Components/WhatsappIcon';
-import BackTop from './Components/BackTop';
-import Page404 from './Containers/Page 404';
-import AdminHome from './Containers/Admin/AdminHome';
-import login from './Containers/Admin/Login';
+// import Service from './Containers/Services';
+// import About from './Containers/About';
+// import Product from './Containers/Products';
+// import Client from './Containers/Clients';
+// import Contact from './Containers/Contact';
+// import Footer from './Components/Footer';
+// import ProductPage from './Components/UI/ProductPage';
+// import WhatsAppIcon from './Components/WhatsappIcon';
+// import BackTop from './Components/BackTop';
+// import Page404 from './Containers/Page 404';
+// import AdminHome from './Containers/Admin/AdminHome';
+// import login from './Containers/Admin/Login';
+const Navbar = lazy(() => import('./Components/Navbar'));
+const Home = lazy(() => import('./Containers/Home'));
+const Service = lazy(() => import('./Containers/Services'));
+const About = lazy(() => import('./Containers/About'));
+const Product = lazy(() => import('./Containers/Products'));
+const Client = lazy(() => import('./Containers/Clients'));
+const Contact = lazy(() => import('./Containers/Contact'));
+const Footer = lazy(() => import('./Components/Footer'));
+const ProductPage = lazy(() => import('./Components/UI/ProductPage'));
+const WhatsAppIcon = lazy(() => import('./Components/WhatsappIcon'));
+const BackTop = lazy(() => import('./Components/BackTop'));
+const Page404 = lazy(() => import('./Containers/Page 404'));
+const AdminHome = lazy(() => import('./Containers/Admin/AdminHome'));
+const login = lazy(() => import('./Containers/Admin/Login'));
 
 function App() {
 
@@ -36,7 +48,11 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Suspense fallback={
+        <div className="divLoader">
+            <img src={loadingImg} alt="Loading" />
+        </div>}>
+        <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/service" component={Service} />
@@ -50,8 +66,9 @@ function App() {
         <Route component={Page404} />
       </Switch>
       <Footer />
-      <BackTop />
       <WhatsAppIcon />
+      <BackTop />
+      </Suspense>
     </div>
   );
 }

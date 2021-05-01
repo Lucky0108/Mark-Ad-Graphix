3823845f238b343b023b75d737957badf56166c5
Lucky0108 <raolakshayyadav02@gmail.com>
2021-05-01 21:00:38 +0530

    Test Push

--- client/src/user/user.js
+++ client/src/user/user.js
@@ -58,20 +58,20 @@ export const authenticate = (data, next) => {
 };
 
 export const isAuthenticated = () => {
-  const cookieToken = getCookie(" token")
+  const cookieToken = getCookie("token")
     console.log(cookieToken)
   if (typeof window == "undefined") {
       return false;
   }
 
-  // if(cookieToken) {
+  if(cookieToken) {
     if (localStorage.getItem("jwt")) {
       return JSON.parse(localStorage.getItem("jwt"));
     } else {
       return false;
     }
-  // } else {
+  } else {
     // localStorage.clear();
-    // return false;
-  // }
+    return false;
+  }
 }
